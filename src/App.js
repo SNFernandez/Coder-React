c487c150ea1e36c25a6313631a7c2306cd5cdadb
Sergio Nicolas Fernandez <sergionicolasfernandez2002@gmail.com>
2023-03-31 18:55:02 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavE from './components/NavE/NavE';
+import ItemListE from './components/ItemsList/ItemList';
+
+function App() {
+  return (
+    <div className="App">
+<NavE/>
+<ItemListE greeting={'Bienvenidos a la 1era pre entrega'}/>
+    </div>
+  );
+}
+
+export default App;
